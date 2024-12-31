4d8fbce044c0ee79bd86dc8deefb3caa9131ecf4
Subhash Sagar Singh <shubhamdev7317@example.com>
2024-12-31 08:33:28 +0530

    delete funtionality added

--- index.js
+++ index.js
@@ -63,13 +63,18 @@ app.patch("/posts/:id", (req, res) =>{
     post.content = newContent;
     console.log(post);
     res.redirect("/posts"); 
- })
+ });
 
  app.get("/posts/:id/edit", (req, res) =>{
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", { post });
- })
+ });
+ app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+     posts = posts.filter((p) => id !== p.id);
+     res.redirect("/posts"); 
+ });
 
 app.listen(port, () =>{
     console.log("Listining to port : 8080");
